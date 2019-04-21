316b3abef901074e4a8ad1283e512d20661550c3
BrainVaibhav Satam <vaibhav.satam@accionlabs.com>
2019-04-21 23:37:45 +0530

    feat(filters): added beam filters

--- beam.py
+++ beam.py
@@ -3,6 +3,7 @@ from apache_beam.options.pipeline_options import SetupOptions
 from apache_beam.io import ReadFromText
 from apache_beam.io import WriteToText
 from apache_beam.metrics import Metrics
+from apache_beam.metrics.metric import MetricsFilter
 import logging
 import argparse
 import re
@@ -11,15 +12,19 @@ import apache_beam as beam
 
 class WordExtractingDoFn(beam.DoFn):
     def __init__(self):
-        pass
+        self.words_counter = Metrics.counter(self.__class__, 'words')
 
     def process(self, element):
         text_line = element.strip()
         words = re.findall(r'[\w\']+', text_line, re.UNICODE)
         
+        for word in words:
+            self.words_counter.inc()
+        
         return words
 
 def count_ones(word_count):
+    
     return word_count
     
 def run(argv=None):
@@ -45,6 +50,11 @@ def run(argv=None):
 
     result = p.run()
     result.wait_until_finish()
+
+    if not hasattr(result, 'has_job'):
+        words = MetricsFilter().with_name('words')
+        query_result = result.metrics().query(words)
+        print(query_result['counters'][0].result)
 
 if __name__ == "__main__":
     logging.getLogger().setLevel(logging.INFO)
